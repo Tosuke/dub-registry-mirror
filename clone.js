56990451580b57546803e69e92d992f8e4c5b7bf
Tosuke <tasukeprg@gmail.com>
2017-04-02 13:11:15 +0900

    clone versions

--- clone.js
+++ clone.js
@@ -1,35 +1,73 @@
-const monk = require("monk");
-const request = require("request-promise-native");
-
-const db = monk("localhost/dub-registry");
-const packages = db.get("packages");
+const mongo = require("monk");
 
 async function main() {
-  const str = await request("http://code.dlang.org/packages/index.json");
-  const list = JSON.parse(str);
-  console.log(list.length);
+  const data = await request("http://code.dlang.org");
+  console.log(`packages: ${data.packages.length}, versions: ${data.versions.length}`);
 
-  let cnt = 1;
+  const db = mongo("localhost/dub-registry");
+  const packages = db.get("packages");
+  const versions = db.get("versions");
 
-  const promises = list.map(async (name) => {
-    try {
-      const obj = JSON.parse(await request(`http://code.dlang.org/api/packages/${name}/info`));
-    
-      console.log(`${name}:${cnt}/${list.length}`);
-    
-      delete obj.id;
-      delete obj.owner;
-      delete obj.versions;
-      packages.insert(obj);
-    } catch(err){}
-    cnt++;
-  });
+  await versions.insert(data.versions);
+  
+  data.packages = await Promise.all(data.packages.map(async (package) => {
+    package.versions = (await versions.find({name: package.name})).map((ver) => ver._id);
+    return package;
+  }));
 
-  await Promise.all(promises);
+  await packages.insert(data.packages);
+  
+  db.close();
 }
 
-main()
-  .then(() => {
-    db.close();
-  });
+main();
 
+async function request(url) {
+  let packages = [];
+  let versions = [];
+
+  const list = await getContent(`${url}/packages/index.json`);
+  console.log(list);
+
+  await Promise.all(list.map(async (name) => {
+    let package = await getContent(`${url}/api/packages/${name}/info`);
+    if(package === undefined) return;
+
+    console.log(name);
+    
+    await Promise.all(package.versions.map(a => a.version).map(async (vername) => {
+      if(/^~/.test(vername)) return;
+      let version = await getContent(`${url}/api/packages/${name}/${vername}/info`);
+      if(version === undefined) return;
+      
+      console.log(`${name}/${vername}`);
+
+      version.name = name;
+      if(version.readme !== undefined) {
+        version.readme = package.versions.filter((v, i, arr) => v.version === vername)[0].readme;
+      }
+      versions.push(version);
+    }));
+
+    delete package.id;
+    delete package.owner;
+    packages.push(package);
+  }));
+
+  return {
+    packages,
+    versions
+  };
+}
+
+
+async function getContent(url) {
+  try {
+    const request = require("request-promise-native");
+
+    const str = await request(url);
+    return JSON.parse(str);
+  } catch(err) {
+    return undefined;
+  }
+}
