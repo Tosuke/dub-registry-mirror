845574d73b0aadd2a88134531486b0fce707c2df
Tosuke <tasukeprg@gmail.com>
2017-04-03 13:09:46 +0900

    implement GET /api/packages/:name/info

--- src/endpoints/api/packages/name/info.ts
+++ src/endpoints/api/packages/name/info.ts
@@ -1,16 +1,32 @@
 import * as koa from "koa";
 import packages from "../../../../models/packages";
+import versions from "../../../../models/versions";
+import {Version} from "../../../../models/versions";
 
 export default async function info(ctx: koa.Context) {
   const name = ctx.params["name"];
   const pkg = await packages.getByName(name);
-  
-  if(pkg !== null) {
-    ctx.body = pkg;
-  } else {
+  if(pkg === null) {
     ctx.status = 404;
     ctx.body = {
       message: "package is not found"
     };
+    return;
+  }
+  
+  const versionInfos = await Promise.all(
+    pkg.versions
+      .map(id => versions.getById(id))
+      .filter(async (ver) => (await ver) !== null)
+      .map(async (ver) => versions.toPackageVersion(<Version>await ver))
+  );
+
+  ctx.body = {
+    repository: pkg.repository,
+    dateAdded: pkg.dateAdded,
+    name: pkg.name,
+    categories: pkg.categories,
+    id: pkg._id,
+    versions: versionInfos
   }
 }
